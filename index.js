e165736b548fd9b1bbb393ee08e317bdcb81be60
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-11-24 01:29:32 +0300

    feat text plain hello server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import http from 'node:http'
 
 const hello = 'Hello Node';
+const PORT = 8080;
+
 console.log(hello);
 
 http
-  .createServer()
-  .listen(8080);
-  
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/plain; charset=utf-8",
+    });
+    res.end('Hello Server');
+  }
+    
+  )
+  .listen(PORT);
