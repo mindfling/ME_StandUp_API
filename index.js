491eb3578856480b347f706949264f225be2c98b
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-11-24 01:31:43 +0300

    feat allow cross origin

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ http
   .createServer((req, res) => {
     res.writeHead(200, {
       "Content-Type": "text/plain; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
     });
     res.end('Hello Server');
   }
