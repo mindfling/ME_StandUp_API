757c81f4cd79c35e4c682624cd8067c60eabc544
Nepochtov Dmitry <dimendroider@gmail.com>
2023-11-29 00:42:27 +0300

    feat note todo

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 import http from 'node:http'
 import fs from 'node:fs'
+import { log } from 'node:console'; //?
 
-const hello = 'Hello Node';
+const hello = 'Hello Node'; //
+log(hello); //?
+
 const PORT = 8080;
 const FILE_PATH = 'comedians.json';
 
-let text = ''
+let text = ''; // todo temp var
 
+// todo читаем файл
 fs.readFile(FILE_PATH, 'utf-8', (err, data) => {
-  // console.log(data);
   text = data;
 })
 
 http
-  .createServer((req, res) => {
+  .createServer((req, res) => { // создаем сервер
+    
+    // заголовок ответа
     res.writeHead(200, {
       "Content-Type": "text/html; charset=utf-8",
       "Access-Control-Allow-Origin": "*",
     });
-    res.end('<body style="font-family:sans-serif;background-color:#1a1a2a;color:lawngreen"><h1>Hello Server</h1><p>our file:</p><pre>' + text + '</pre></body>');
+
+    // ответ сервера
+    res.end(`<body style="font-family:sans-serif;background-color:#1a1a2a;color:lawngreen">
+      <h1>Hello Server</h1>
+      <p>our file:</p>
+      <pre>${text}</pre>
+    </body>`);
     // res.end(text);
   }
-    
+
   )
-  .listen(PORT);
-  
+  .listen(PORT); // слушаем порт
+
 console.log('Сервер запущен на порту', PORT);
